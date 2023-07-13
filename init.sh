022fc4b486634a3edf2bf0867c9c02fcf1d458a6
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-13 22:14:47 +0100

    small refactoring

--- init.sh
+++ init.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 if [[ $1 == '' ]]
 then
-  echo "Error: Please enter a name for your new sudo user."
+  echo "Error: Please provide a name for your new sudo user."
   exit
 fi
 
-echo "Updating package index"
 apt-get update -y
 
-echo "Creating user"
 adduser --disabled-password --gecos "" --quiet "$1"
-
-echo "User $1 created. Granting sudo privileges"
 usermod -aG sudo "$1"
-
 rsync --archive --chown="$1":"$1" ~/.ssh /home/"$1"
 
-echo "Enabling Firewall and allowing OpenSSH"
 sudo ufw --force enable
 ufw allow OpenSSH
