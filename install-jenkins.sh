7955568db63e1c9451e1510ce56a4f423b384c75
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-14 18:38:18 +0100

    jenkins install script

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-sudo apt install default-jre default-jdk
+sudo apt install default-jre default-jdk -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo ufw allow 8080
+
+echo -e "Jenkins installed and available on port 8080. Initial admin password:"
+cat /var/lib/jenkins/secrets/initialAdminPassword
