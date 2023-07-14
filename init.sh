85eb504d7ad341c768eecaca455c722c51fa965a
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-14 17:45:36 +0100

    added script output

--- init.sh
+++ init.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 if [[ $1 == '' ]]
 then
-  echo "Error: Please provide a name for your new sudo user."
+  echo -e "Error: Please provide a name for your new sudo user."
   exit
 fi
 
-echo "Updating package list"
+echo -e "Updating package list"
 apt-get update -y
 
-echo "Creating new user with username $1"
+echo -e "Creating new user with username $1"
 adduser --disabled-password --gecos "" --quiet "$1"
 
-echo "Granting sudo privileges to $1"
+echo -e "Granting sudo privileges to $1"
 usermod -aG sudo "$1"
 rsync --archive --chown="$1":"$1" ~/.ssh /home/"$1"
 
-echo "Enabling firewall"
+echo -e "Enabling firewall"
 sudo ufw --force enable
 
-echo "Allowing OpenSSH"
+echo -e "Allowing OpenSSH"
 ufw allow OpenSSH
 
 echo "User created, please exit your server and connect again with $1"
