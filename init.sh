683e4573c7ccbcb4e2896a1df8bc711163af2aae
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-14 17:43:11 +0100

    added script output

--- init.sh
+++ init.sh
@@ -5,11 +5,20 @@ then
   exit
 fi
 
+echo "Updating package list"
 apt-get update -y
 
+echo "Creating new user with username $1"
 adduser --disabled-password --gecos "" --quiet "$1"
+
+echo "Granting sudo privileges to $1"
 usermod -aG sudo "$1"
 rsync --archive --chown="$1":"$1" ~/.ssh /home/"$1"
 
+echo "Enabling firewall"
 sudo ufw --force enable
+
+echo "Allowing OpenSSH"
 ufw allow OpenSSH
+
+echo "User created, please exit your server and connect again with $1"
